895103706e9c1e0504b4c0b330a12e531d2d326f
Seiji-U <seiji.uehara@fabricgroup.com.au>
2018-05-10 13:31:19 +1000

    third

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
-    currentTime.setTimezone("Australia/Melbourne");
+    currentTime.setTimezone("Australia/Sydney");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Melbourne is: ' + currentTime.toString(),
+        body: 'The time in Sydney is: ' + currentTime.toString(),
     });
 };
